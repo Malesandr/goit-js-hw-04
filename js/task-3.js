bc5db259462acff4011817730917de5a761647ea
Malesandr <aleksandr.mikolenko@gmail.com>
2023-11-23 23:58:24 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,21 @@
-function filterArray(numbers, value) {
-    const array = [];
-          for(let i = 0; i <= numbers.length; i ++) {
-            if(numbers[i] > value) {
-              array.push(numbers[i]);
-            }
-          }
-      return array;
-    }
-    
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  
+ changeUsername(newName) {
+  this.username = newName;
+},
+ updatePlayTime(hours) {
+  this.playTime += hours;
+},
+ getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`
+}
+}
 
 
-
-    console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-    profile.changeUsername("Marco");
-    console.log(profile.getInfo()); // "Marco has 300 active hours!"
-    
-    profile.updatePlayTime(20);
-    console.log(profile.getInfo()); // "Marco has 320 active hours!"
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
